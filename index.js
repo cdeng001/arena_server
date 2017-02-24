e7a7e2b650d5d592342ceebc7834f9e9460c844c
cdeng001 <cdeng001@ucr.edu>
2017-02-23 21:48:08 -0800

    minor clean up

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 var net = require('net');
 
 var server = net.createServer(function(socket) {
+    console.log("client connecting . . . ");
     socket.write('Echo server');
     socket.pipe(socket);
 });
 
-server.listen(3000, '127.0.0.1');
+server.listen(3000);
