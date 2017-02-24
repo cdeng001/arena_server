34a06e4d07d64fc1b7fcb567473525cd17de03ce
cdeng001 <cdeng001@ucr.edu>
2017-02-24 01:22:50 -0800

    package

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 // content of index.js
 var net = require('net');
 
-var server = net.createServer(function(socket) {
-    console.log("client connecting . . . ");
-    socket.write('Echo server');
-    socket.pipe(socket);
-});
+var server = net.createServer();
+server.on('connection', handleConnection);
 
-server.listen(3000);
+function handleConnection(conn){
+    var remote = conn.remoteAddress + ':' + conn.remotePort;
+    console.log("client connected from " + remote);
+
+    conn.on('data', onConnData);
+    conn.once('close', onConnClose);
+    conn.on('error', onConnError);
+
+    function onConnData(d) {
+        var j = JSON.parse(d);
+        console.log('connection data from %s: %j', remote, d);
+        console.log(j);
+        conn.write(d);
+    }
+
+    function onConnClose() {
+        console.log('connection from %s closed', remote);
+    }
+
+    function onConnError(err) {
+        console.log('Connection %s error: %s', remote, err.message);
+    }
+}
+
+server.listen(3000, function(){
+    console.log("server listening on port 3000.");
+});
